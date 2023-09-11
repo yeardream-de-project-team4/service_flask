c84caff48c1a011066ce34f5a9271a7a2dc32958
dungnss <38428357+dbin67@users.noreply.github.com>
2023-09-11 14:28:19 +0900

    Update consumer_weather.py

--- consumers/scripts/consumer_weather.py
+++ consumers/scripts/consumer_weather.py
@@ -3,4 +3,4 @@ import requests
 
 def callback_weather(message):
     num = message.value["num"]  # {'num': '20'}
-    requests.post(f"http://haproxy:80/taehoon/do/{num}")
+    requests.post(f"http://haproxy:80/weather/do/{num}")
